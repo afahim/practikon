12e4a9faf3f1a96ceaa981af027cb89aecd22bda
Afnan Fahim <afahim@qatar.cmu.edu>
2013-07-10 14:34:23 +0300

    made choices aligned with arrows; minor changes to text appearance

---
+++ v0.8-concision/js/myjs/practikon.js
@@ -0,0 +1,14 @@
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    switch (evt.keyCode) {
+        case 13: //Enter
+        	
+        	break;
+        case 37: //Left Key
+
+            break;
+        case 39: //Right Key
+
+            break;
+    }
+};
