ab56da2718801dcdc615f9927e99bf67f3e5b1b5
Afnan Fahim <afahim@qatar.cmu.edu>
2013-07-14 20:03:31 +0300

    updated project setup

---
+++ v0.10-nagivation-1/js/myjs/practikon.js
@@ -0,0 +1,26 @@
+var scopeDismissed = true;
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    switch (evt.keyCode) {
+        case 13: //Enter
+        	submitResponse();
+        	break;
+        case 37: //Left Key
+        	if (!scopeDismissed)
+        	{
+        		window.mySwipe.prev();
+        	}
+            break;
+        case 39: //Right Key
+        	if (!scopeDismissed)
+        	{
+        		window.mySwipe.next();
+        	}
+            break;
+    }
+};
+
+function submitResponse() {
+	$(".problematic").addClass("submitted");
+}
